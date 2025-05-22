5043162d0f2eaba10fc9ec08d9f21b5d23ed3363
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2025-05-22 14:28:35 +0330

    creating user and user authentication

--- src/main/java/org/example/wallet/WalletApplication.java
+++ src/main/java/org/example/wallet/WalletApplication.java
@@ -3,8 +3,12 @@ package org.example.wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class WalletApplication {
@@ -16,5 +20,27 @@ public class WalletApplication {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authz -> authz.requestMatchers("/login",
+                                "/logout",
+                                "/register",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                                , "/swagger-ui/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )).formLogin(form -> form.defaultSuccessUrl("/wallet/balance", true))
+                .logout(logout -> logout.permitAll());
+        return http.build();
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
     }
 }
